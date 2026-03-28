1cf2a74467aeaa3261aff98535ca0c9b97f2eb84
janegil-staff <janstovr@icloud.com>
2026-03-28 13:22:19 +0100

    Add suppressHydrationWarning to layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,6 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {" "}
         <LangProvider>{children}</LangProvider>
       </body>
     </html>
